0c841644a53137da1c08ef8d7bedcf2333efaf72
Dmitry Sichkar <dmmeteo@gmail.com>
2018-06-05 15:03:01 +0300

    refactor runner

--- run.py
+++ run.py
@@ -1,30 +1,30 @@
 # -*- coding: utf-8 -*-
 
-from glob import glob
-import os
-import sys
+
 import threading
 import time
 import schedule
 from instabot import Bot, utils
 import config
+from tqdm import tqdm
 
 
 bot = Bot(comments_file=config.COMMENTS_FILE,
-          blacklist_file=config.BLACKLIST_FILE,
-          whitelist_file=config.WHITELIST_FILE,
           friends_file=config.FRIENDS_FILE)
 bot.login()
-bot.logger.info("ULTIMATE script. Safe to run 24/7!")
+bot.logger.info("Instagram bot. Safe to run 24/7!")
 
 random_user_file = utils.file(config.USERS_FILE)
 random_hashtag_file = utils.file(config.HASHTAGS_FILE)
-photo_captions_file = utils.file(config.PHOTO_CAPTIONS_FILE)
-posted_pic_list = utils.file(config.POSTED_PICS_FILE).list
 
-pics = sorted([os.path.basename(x) for x in
-               glob(config.PICS_PATH + "/*.jpg")])
-
+pbar1 = tqdm(
+    total=config.NUMBER_OF_NON_FOLLOWERS_TO_UNFOLLOW,
+    desc='To unfollow'
+)
+pbar2 = tqdm(
+    total=config.NUMBER_OF_FOLLOWERS_TO_FOLLOW,
+    desc='To follow'
+)
 
 def stats():
     bot.save_user_stats(bot.user_id)
@@ -51,8 +51,12 @@ def comment_medias():
 
 
 def unfollow_non_followers():
-    bot.unfollow_non_followers(n_to_unfollows=config.NUMBER_OF_NON_FOLLOWERS_TO_UNFOLLOW)
-
+    n_to_unfollows = config.NUMBER_OF_NON_FOLLOWERS_TO_UNFOLLOW
+    non_followers = set(bot.following) - set(bot.followers) - bot.friends_file.set
+    non_followers = list(non_followers)
+    for user_id in non_followers[:n_to_unfollows]:
+        bot.api.unfollow(user_id)
+        bot.delay('unfollow')
 
 def follow_users_from_hastag_file():
     bot.follow_users(bot.get_hashtag_users(random_hashtag_file.random()))
@@ -64,53 +68,6 @@ def comment_hashtag():
     bot.comment_hashtag(hashtag)
 
 
-def upload_pictures():  # Automatically post a pic in 'pics' folder
-    try:
-        for pic in pics:
-            if pic in posted_pic_list:
-                continue
-
-            caption = photo_captions_file.random()
-            full_caption = caption + "\n" + config.FOLLOW_MESSAGE
-            bot.logger.info("Uploading pic with caption: " + caption)
-            bot.upload_photo(config.PICS_PATH + pic, caption=full_caption)
-            if bot.last_response.status_code != 200:
-                bot.logger.error("Something went wrong, read the following ->\n")
-                bot.logger.error(bot.last_response)
-                break
-
-            if pic not in posted_pic_list:
-                # After posting a pic, comment it with all the hashtags specified
-                # In config.PICS_HASHTAGS
-                posted_pic_list.append(pic)
-                with open('pics.txt', 'a') as f:
-                    f.write(pic + "\n")
-                bot.logger.info("Succesfully uploaded: " + pic)
-                bot.logger.info("Commenting uploaded photo with hashtags...")
-                medias = bot.get_your_medias()
-                last_photo = medias[0]  # Get the last photo posted
-                bot.comment(last_photo, config.PICS_HASHTAGS)
-                break
-    except Exception as e:
-        bot.logger.error("Couldn't upload pic")
-        bot.logger.error(str(e))
-
-
-def put_non_followers_on_blacklist():  # put non followers on blacklist
-    try:
-        bot.logger.info("Creating non-followers list")
-        followings = set(bot.following)
-        followers = set(bot.followers)
-        friends = bot.friends_file.set  # same whitelist (just user ids)
-        non_followers = followings - followers - friends
-        for user_id in non_followers:
-            bot.blacklist_file.append(user_id, allow_duplicates=False)
-        bot.logger.info("Done.")
-    except Exception as e:
-        bot.logger.error("Couldn't update blacklist")
-        bot.logger.error(str(e))
-
-
 def run_threaded(job_fn):
     job_thread = threading.Thread(target=job_fn)
     job_thread.start()
@@ -120,14 +77,6 @@ schedule.every(1).hour.do(run_threaded, stats)
 schedule.every(1).minutes.do(run_threaded, like_hashtags)
 schedule.every(1).minutes.do(run_threaded, unfollow_non_followers)
 schedule.every(1).minutes.do(run_threaded, follow_users_from_hastag_file)
-
-# schedule.every(2).hours.do(run_threaded, like_timeline)
-# schedule.every(1).days.at("16:00").do(run_threaded, like_followers_from_random_user_file)
-# schedule.every(2).days.at("11:00").do(run_threaded, follow_followers)
-# schedule.every(10).minutes.do(run_threaded, comment_medias)
-# schedule.every(6).hours.do(run_threaded, comment_hashtag)
-# schedule.every(1).days.at("21:28").do(run_threaded, upload_pictures)
-# schedule.every(4).days.at("07:50").do(run_threaded, put_non_followers_on_blacklist)
 
 while True:
     schedule.run_pending()
